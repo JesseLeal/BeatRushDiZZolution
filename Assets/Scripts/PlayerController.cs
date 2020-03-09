f365af3b5dd4d382b43ba68d28eda5e554fe6b5a
Jesse Leal <38059219+JesseLeal@users.noreply.github.com>
2020-03-08 22:24:21 -0600

    I hate

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] float m_speed = 100.0f;
+    [SerializeField] float m_invincibleTime = 2.0f;
+
+    private bool m_left = false;
+    private bool m_right = false;
+
+    void Start()
+    {
+        
+    }
+    
+    void Update()
+    {
+        m_left = false;
+        m_right = false;
+
+        //debug
+        if (Application.platform == RuntimePlatform.WindowsEditor)
+        {
+            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+            {
+                m_left = true;
+            }
+            if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+            {
+                m_right = true;
+            }
+        }
+
+        //Android
+        else if (Application.platform == RuntimePlatform.Android)
+        {
+            if (PlayerPrefs.GetInt("TiltControls", 0) == 0)
+            {
+                //touch controls
+                foreach (Touch touch in Input.touches)
+                {
+                    if (touch.position.x < Screen.width / 2)
+                    {
+                        m_left = true;
+                    }
+
+                    else
+                    {
+                        m_right = true;
+                    }
+                }
+            }
+
+            else
+            {
+                //tilt controls
+                if (Input.acceleration.x > 0.05f)
+                {
+                    m_right = true;
+                }
+                else if (Input.acceleration.x < -0.05f)
+                {
+                    m_left = true;
+                }
+            }
+        }
+
+        if (m_left && !m_right)
+        {
+            transform.position -= new Vector3(m_speed, 0.0f, 0.0f) * Time.deltaTime;
+        }
+        else if (m_right && !m_left)
+        {
+            transform.position += new Vector3(m_speed, 0.0f, 0.0f) * Time.deltaTime;
+        }
+
+        transform.position = new Vector3(Mathf.Clamp(transform.position.x, -9.5f, 9.5f), transform.position.y, 0.0f);
+    }
+
+    public void Hit()
+    {
+        StartCoroutine(HitRoutine());
+    }
+
+    private IEnumerator HitRoutine()
+    {
+        //play the I've been hit sound, make them flash, come back in a fixed time
+        GetComponent<Collider2D>().enabled = false;
+
+        yield return new WaitForSeconds(m_invincibleTime);
+
+        GetComponent<Collider2D>().enabled = true;
+    }
+}
---
+++ Assets/Scripts/ShipEnemy.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipEnemy : MonoBehaviour
+{
+    [SerializeField] int m_scorePenalty = 200;
+    [SerializeField] float m_speed = 1.0f;
+    [SerializeField] GameObject m_player = null;
+
+    void Start()
+    {
+        if (m_player == null)
+        {
+            m_player = GameObject.FindGameObjectWithTag("Player");
+        }
+    }
+    
+    void Update()
+    {
+        float targetX = m_player.transform.position.x - transform.position.x;
+        if (targetX >= 1.0f)
+        {
+            //move right
+            transform.rotation = Quaternion.AngleAxis(30.0f, Vector3.forward);
+        }
+        else if (targetX <= -1.0f)
+        {
+            //move left
+            transform.rotation = Quaternion.AngleAxis(-30.0f, Vector3.forward);
+        }
+        else
+        {
+            //move down
+            transform.rotation = Quaternion.AngleAxis(0.0f, Vector3.forward);
+        }
+
+        transform.position -= transform.up * m_speed * Time.deltaTime;
+
+        if (transform.position.y <= -5.5f)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            //then we hit a player, tell the game to deduct them
+            Game.Instance.HitPlayer(m_scorePenalty);
+        }
+    }
+}
